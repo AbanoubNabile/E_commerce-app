f2aef62e58a1218d61092b444e1f1ca518f28701
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-24 00:12:43 +0200

    connect to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:commec_app/pages/login.dart';
+import 'package:commec_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Login());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Register());
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:commec_app/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:commec_app/shared/colors.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +6,8 @@ import 'package:flutter/material.dart';
 import '../shared/colors.dart';
 import '../shared/constant.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Register extends StatelessWidget {
+  const Register({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,54 +16,69 @@ class Login extends StatelessWidget {
           body: Center(
         child: Padding(
           padding: const EdgeInsets.all(33.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 70,
-              ),
-              TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: widget_text_felid.copyWith(
-                    hintText: "Enter your email",
-                  )),
-              const SizedBox(height: 30),
-              TextField(
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: widget_text_felid.copyWith(
-                    hintText: "Enter your password",
-                  )),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(BTNgreen),
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8))),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 70,
                 ),
-                child: const Text(
-                  "Sign in",
-                  style: TextStyle(fontSize: 19),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: false,
+                    decoration: widget_text_felid.copyWith(
+                      hintText: "Enter your userName",
+                    )),
+                const SizedBox(height: 30),
+                TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: widget_text_felid.copyWith(
+                      hintText: "Enter your email",
+                    )),
+                const SizedBox(height: 30),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: widget_text_felid.copyWith(
+                      hintText: "Enter your password",
+                    )),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(fontSize: 19),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Do not have an account ?",
-                      style: TextStyle(fontSize: 20)),
-                  TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "ٍٍSign Up",
-                        style: TextStyle(color: Colors.black),
-                      ))
-                ],
-              )
-            ],
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Do not have an account ?",
+                        style: TextStyle(fontSize: 20)),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Login()));
+                        },
+                        child: const Text(
+                          "ٍٍSign In",
+                          style: TextStyle(color: Colors.black),
+                        ))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       )),
