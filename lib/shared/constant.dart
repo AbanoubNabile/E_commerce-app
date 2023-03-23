729aeecea93f25170b74e574005e08d2ad6e34d0
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-23 22:31:01 +0200

    edit boder side

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
-
-
-
-const widget =InputDecoration(
-                  hintText: "Enter your email",
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey)),
-                  filled: true,
-                  contentPadding: const EdgeInsets.all(8)))
+const widget = InputDecoration(
+    hintText: "Enter your email",
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+        filled: true,
+        contentPadding: const EdgeInsets.all(8)));
