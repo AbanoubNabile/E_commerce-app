1f87298dea4cffcd8087ec6207ec18c371da4335
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-23 20:52:21 +0200

    add text field password

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -22,18 +22,21 @@ class Login extends StatelessWidget {
                   focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.grey)),
                   filled: true,
+                  contentPadding: const EdgeInsets.all(8))),
+          const SizedBox(height: 30),
+          TextField(
+              keyboardType: TextInputType.emailAddress,
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: "Enter your password  ",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)),
+                  filled: true,
                   contentPadding: const EdgeInsets.all(8)))
         ],
       ),
     ));
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
