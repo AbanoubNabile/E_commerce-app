ecffc6fdfaecdfa392f65fe040efbe658c9893bd
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-23 21:04:08 +0200

    create  custom textfiled  widget

--- lib/constant/constant_text_field.dart
+++ lib/constant/constant_text_field.dart
@@ -1,1 +1,20 @@
-stl
+import 'package:flutter/material.dart';
+
+class custom_textfiled extends StatelessWidget {
+  const custom_textfiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: false,
+        decoration: InputDecoration(
+            hintText: "Enter your Email ",
+            enabledBorder: OutlineInputBorder(
+                borderSide: Divider.createBorderSide(context)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            filled: true,
+            contentPadding: const EdgeInsets.all(8)));
+  }
+}
