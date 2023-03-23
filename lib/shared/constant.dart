7242f14c893af42a2363b869f501e7604d563209
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-23 23:16:19 +0200

    create file colors

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -8,7 +8,3 @@ const widget_text_felid = InputDecoration(
         OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
     filled: true,
     contentPadding: const EdgeInsets.all(8));
-
-const BTNpink = Color.fromARGB(255, 241, 39, 100);
-const BTNgreen = Color.fromARGB(255, 73, 179, 105);
-const appbarGreen = Color.fromARGB(255, 76, 141, 95);
