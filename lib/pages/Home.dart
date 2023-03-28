0e639ad89a7e37dd148bbd1916b8f30768932d4c
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 01:30:48 +0200

    add appBar

---
+++ lib/pages/Home.dart
@@ -0,0 +1,14 @@
+import 'package:commec_app/pages/login.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: 
+    );
+  }
+}
