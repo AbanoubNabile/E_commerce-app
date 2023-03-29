08bb8be50bf4dc0c5637515877b41a263db227aa
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 20:34:27 +0200

    add location

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -1,17 +1,21 @@
 class Item {
   String imgPath;
   double price;
+  String location;
 
-  Item({required this.imgPath, required this.price});
+  Item(
+      {required this.imgPath,
+      required this.price,
+      this.location = "Main branch"});
 }
 
 final List<Item> items = [
-  Item(price: 612.99, imgPath: "assets/img/1.webp"),
-  Item(price: 112.99, imgPath: "assets/img/2.webp"),
-  Item(price: 125.99, imgPath: "assets/img/3.webp"),
-  Item(price: 129.99, imgPath: "assets/img/4.webp"),
-  Item(price: 127.99, imgPath: "assets/img/5.webp"),
-  Item(price: 120.99, imgPath: "assets/img/6.webp"),
-  Item(price: 122.99, imgPath: "assets/img/7.webp"),
-  Item(price: 121.99, imgPath: "assets/img/8.webp"),
+  Item(price: 12.99, imgPath: "assets/img/1.webp", location: "Abanoub shop"),
+  Item(price: 12.99, imgPath: "assets/img/2.webp", location: "Nabil"),
+  Item(price: 12.99, imgPath: "assets/img/3.webp"),
+  Item(price: 12.99, imgPath: "assets/img/4.webp"),
+  Item(price: 12.99, imgPath: "assets/img/5.webp"),
+  Item(price: 12.99, imgPath: "assets/img/6.webp"),
+  Item(price: 12.99, imgPath: "assets/img/7.webp"),
+  Item(price: 12.99, imgPath: "assets/img/8.webp"),
 ];
