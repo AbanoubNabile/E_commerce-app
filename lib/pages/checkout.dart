56faa79808cb1695f6cca2613740aae2239c8ead
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 22:32:30 +0200

    create checkOut widget

---
+++ lib/pages/checkout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class checkOut extends StatelessWidget {
+  const checkOut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
