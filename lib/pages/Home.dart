daa5bd0a1645ac879a38ab37c11253817c54ac67
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 16:19:29 +0200

    create body

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,4 @@
 import 'package:commec_app/shared/colors.dart';
-import 'dart:ui' show lerpDouble;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,38 +9,43 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        body: ,
         drawer: Drawer(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("assets\img\686034.jpg"),
-                          fit: BoxFit.cover)),
-                  currentAccountPicture: CircleAvatar(
-                      radius: 55,
-                      backgroundImage:
-                          AssetImage("assets\img\IMG_20230302_134818.jpg")),
-                  accountEmail: Text("Abanoub Nabil @gmail.com"),
-                  accountName: Text("Abanoub Nabil"),
+                Column(
+                  children: [
+                    UserAccountsDrawerHeader(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets\img\686034.jpg"),
+                              fit: BoxFit.cover)),
+                      currentAccountPicture: CircleAvatar(
+                          radius: 55,
+                          backgroundImage:
+                              AssetImage("assets\img\IMG_20230302_134818.jpg")),
+                      accountEmail: Text("Abanoub Nabil @gmail.com"),
+                      accountName: Text("Abanoub Nabil"),
+                    ),
+                    ListTile(
+                        title: Text("Home"),
+                        leading: Icon(Icons.home),
+                        onTap: () {}),
+                    ListTile(
+                        title: Text("My products"),
+                        leading: Icon(Icons.add_shopping_cart),
+                        onTap: () {}),
+                    ListTile(
+                        title: Text("About"),
+                        leading: Icon(Icons.help_center),
+                        onTap: () {}),
+                    ListTile(
+                        title: Text("Logout"),
+                        leading: Icon(Icons.exit_to_app),
+                        onTap: () {}),
+                  ],
                 ),
-                ListTile(
-                    title: Text("Home"),
-                    leading: Icon(Icons.home),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("My products"),
-                    leading: Icon(Icons.add_shopping_cart),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("About"),
-                    leading: Icon(Icons.help_center),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("Logout"),
-                    leading: Icon(Icons.exit_to_app),
-                    onTap: () {}),
                 Container(
                   margin: EdgeInsets.only(bottom: 12),
                   child: Text("Developed by Abanoub Nabil © 2023",
