a6105b904d163774a688c8fe7d45f8edd3645793
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 15:47:28 +0200

    add list title my products

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -10,56 +10,60 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          drawer: Drawer(
-            child: Column(children: [
-              UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets\img\686034.jpg"),
-                        fit: BoxFit.cover)),
-                currentAccountPicture: CircleAvatar(
-                    radius: 55,
-                    backgroundImage:
-                        AssetImage("assets\img\IMG_20230302_134818.jpg")),
-                accountEmail: Text("Abanoub Nabil @gmail.com"),
-                accountName: Text("Abanoub Nabil"),
-              ),
-              ListTile(
-                  title: Text("Home"), leading: Icon(Icons.home), onTap: () {})
-            ]),
-          ),
-          appBar: AppBar(
-            actions: [
-              Row(
-                children: [
-                  Stack(
-                    children: [
-                      Positioned(
-                        bottom: 24,
-                        child: Container(
-                            child: Text("8",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Color.fromARGB(255, 0, 0, 0))),
-                            decoration: BoxDecoration(
-                                color: Color.fromARGB(211, 164, 255, 193),
-                                shape: BoxShape.circle)),
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.add_shopping_cart)),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 12),
-                    child: Text("\$13", style: TextStyle(fontSize: 18)),
-                  ),
-                ],
-              ),
-            ],
-            backgroundColor: appbarGreen,
-            title: Text("Home"),
-          )),
+        drawer: Drawer(
+          child: Column(children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets\img\686034.jpg"),
+                      fit: BoxFit.cover)),
+              currentAccountPicture: CircleAvatar(
+                  radius: 55,
+                  backgroundImage:
+                      AssetImage("assets\img\IMG_20230302_134818.jpg")),
+              accountEmail: Text("Abanoub Nabil @gmail.com"),
+              accountName: Text("Abanoub Nabil"),
+            ),
+            ListTile(
+                title: Text("Home"), leading: Icon(Icons.home), onTap: () {}),
+            ListTile(
+                title: Text("My products"),
+                leading: Icon(Icons.add_shopping_cart),
+                onTap: () {}),
+          ]),
+        ),
+        appBar: AppBar(
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 24,
+                      child: Container(
+                          child: Text("8",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color.fromARGB(255, 0, 0, 0))),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text("\$13", style: TextStyle(fontSize: 18)),
+                ),
+              ],
+            ),
+          ],
+          backgroundColor: appbarGreen,
+          title: Text("Home"),
+        ),
+      ),
     );
   }
 }
