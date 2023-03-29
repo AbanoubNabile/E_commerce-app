1a8b5b0a7f92aa2da5265268ade6e4423307f3cd
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 16:22:55 +0200

    create  gridvew builder

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -9,7 +9,16 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: ,
+        body: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 33),
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return Container();
+            }),
         drawer: Drawer(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
