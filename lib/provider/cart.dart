01af1c51c6a4e8f90dd0c95d4c5274f92521d10b
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 21:09:47 +0200

    add item product

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -4,4 +4,9 @@ import 'package:flutter/material.dart';
 class Cart with ChangeNotifier {
   List selectedProducts = [];
   int price = 0;
+  add(Item product) {
+    selectedProducts.add(product);
+    price += product.price.round();
+    notifyListeners();
+  }
 }
