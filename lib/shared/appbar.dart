51d2bd42401d26ec389bbbd82ef6b765d632f6c1
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 21:50:46 +0200

    create product_and_price widget

--- lib/shared/appbar.dart
+++ lib/shared/appbar.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class product_and_price extends StatelessWidget {
+  const product_and_price({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
