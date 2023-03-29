fdc03d57ae44b41b96c8f6686f14e1e5fe9d16d7
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 02:56:50 +0200

    create drawer

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -12,36 +12,39 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          drawer: Drawer(),
           appBar: AppBar(
-        actions: [
-          Row(
-            children: [
-              Stack(
+            actions: [
+              Row(
                 children: [
-                  Positioned(
-                    child: Container(
-                        child: Text("8",
-                            style: TextStyle(
-                                fontSize: 18,
-                                color: Color.fromARGB(255, 0, 0, 0))),
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(211, 164, 255, 193),
-                            shape: BoxShape.circle)),
+                  Stack(
+                    children: [
+                      Positioned(
+                        bottom: 24,
+                        child: Container(
+                            child: Text("8",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Color.fromARGB(255, 0, 0, 0))),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(211, 164, 255, 193),
+                                shape: BoxShape.circle)),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart)),
+                    ],
                   ),
-                  IconButton(
-                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Text("\$13", style: TextStyle(fontSize: 18)),
+                  ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: Text("\$13", style: TextStyle(fontSize: 18)),
-              ),
             ],
-          ),
-        ],
-        backgroundColor: appbarGreen,
-        title: Text("Home"),
-      )),
+            backgroundColor: appbarGreen,
+            title: Text("Home"),
+          )),
     );
   }
 }
