f62f5753b65057299ea00c5952643a34b8a2aaa6
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-03-29 15:57:34 +0200

    add space

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -11,34 +11,43 @@ class Home extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         drawer: Drawer(
-          child: Column(children: [
-            UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets\img\686034.jpg"),
-                      fit: BoxFit.cover)),
-              currentAccountPicture: CircleAvatar(
-                  radius: 55,
-                  backgroundImage:
-                      AssetImage("assets\img\IMG_20230302_134818.jpg")),
-              accountEmail: Text("Abanoub Nabil @gmail.com"),
-              accountName: Text("Abanoub Nabil"),
-            ),
-            ListTile(
-                title: Text("Home"), leading: Icon(Icons.home), onTap: () {}),
-            ListTile(
-                title: Text("My products"),
-                leading: Icon(Icons.add_shopping_cart),
-                onTap: () {}),
-            ListTile(
-                title: Text("About"),
-                leading: Icon(Icons.help_center),
-                onTap: () {}),
-            ListTile(
-                title: Text("Logout"),
-                leading: Icon(Icons.exit_to_app),
-                onTap: () {}),
-          ]),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets\img\686034.jpg"),
+                          fit: BoxFit.cover)),
+                  currentAccountPicture: CircleAvatar(
+                      radius: 55,
+                      backgroundImage:
+                          AssetImage("assets\img\IMG_20230302_134818.jpg")),
+                  accountEmail: Text("Abanoub Nabil @gmail.com"),
+                  accountName: Text("Abanoub Nabil"),
+                ),
+                ListTile(
+                    title: Text("Home"),
+                    leading: Icon(Icons.home),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("My products"),
+                    leading: Icon(Icons.add_shopping_cart),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("About"),
+                    leading: Icon(Icons.help_center),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("Logout"),
+                    leading: Icon(Icons.exit_to_app),
+                    onTap: () {}),
+                Container(
+                  margin: EdgeInsets.only(bottom: 12),
+                  child: Text("Developed by Abanoub Nabil © 2023",
+                      style: TextStyle(fontSize: 16)),
+                )
+              ]),
         ),
         appBar: AppBar(
           actions: [
